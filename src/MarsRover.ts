117781016b61a903ef0450969d82b73cacd286b7
Fernando García Hernández <86410308+acidfernando@users.noreply.github.com>
2024-03-15 16:36:22 +0000

    green: rotates left and move forward (gets out of grid)

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -1,36 +1,43 @@
 export class MarsRover {
-
-  private facingDirection: string = 'N'
+  private facingDirection: string = "N"
   private xPosition: number = 0
   private yPosition: number = 0
 
   constructor() {}
 
+  private moveLeft() {
+    if (this.xPosition === 0) {
+      this.xPosition = 9
+      return
+    }
+
+    this.xPosition -= 1
+  }
+
   public execute(command: string): string {
-    
-    command.split('').forEach((instruction) => {
-      switch(instruction) {
-        case 'M':
-          if (this.facingDirection == 'N') this.yPosition++
-          else if (this.facingDirection == 'E') this.xPosition++
-          else if (this.facingDirection == 'S') this.yPosition--
-          else if (this.facingDirection == 'W') this.xPosition--
-          break;
-        case 'L':
-          if (this.facingDirection == 'N') this.facingDirection = 'W'
-          else if (this.facingDirection == 'E') this.facingDirection = 'N'
-          else if (this.facingDirection == 'S') this.facingDirection = 'E'
-          else if (this.facingDirection == 'W') this.facingDirection = 'S'
-          break;
-        case 'R':
-          if (this.facingDirection == 'N') this.facingDirection = 'E'
-          else if (this.facingDirection == 'E') this.facingDirection = 'S'
-          else if (this.facingDirection == 'S') this.facingDirection = 'W'
-          else if (this.facingDirection == 'W') this.facingDirection = 'N'
-          break;
+    command.split("").forEach((instruction) => {
+      switch (instruction) {
+        case "M":
+          if (this.facingDirection == "N") this.yPosition++
+          else if (this.facingDirection == "E") this.xPosition++
+          else if (this.facingDirection == "S") this.yPosition--
+          else if (this.facingDirection == "W") this.moveLeft()
+          break
+        case "L":
+          if (this.facingDirection == "N") this.facingDirection = "W"
+          else if (this.facingDirection == "E") this.facingDirection = "N"
+          else if (this.facingDirection == "S") this.facingDirection = "E"
+          else if (this.facingDirection == "W") this.facingDirection = "S"
+          break
+        case "R":
+          if (this.facingDirection == "N") this.facingDirection = "E"
+          else if (this.facingDirection == "E") this.facingDirection = "S"
+          else if (this.facingDirection == "S") this.facingDirection = "W"
+          else if (this.facingDirection == "W") this.facingDirection = "N"
+          break
       }
     })
-    
+
     return `${this.xPosition}:${this.yPosition}:${this.facingDirection}`
   }
 }
