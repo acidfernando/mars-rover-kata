b91ebc1750edc4704d3817db30787557b62efc25
Manuel Andrés Carrera Galafate <manuelcarrera@acidtango.com>
2024-03-15 16:07:58 +0000

    refactor: tests naming

--- src/MarsRover.spec.ts
+++ src/MarsRover.spec.ts
@@ -2,35 +2,35 @@ import { describe, expect, it } from "vitest"
 import { MarsRover } from "./MarsRover.js"
 
 describe(MarsRover, () => {
-  it("should create a Mars Rover in 0:0:N", () => {
+  it("is created in 0:0:N", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("")
     expect(position).toBe("0:0:N")
   })
 
-  it("should create a rover and move it North", () => {
+  it("moves it North", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("M")
     expect(position).toBe("0:1:N")
   })
 
-  it("should create a rover and should be able to turn Left", () => {
+  it("turns Left", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("L")
     expect(position).toBe("0:0:W")
   })
 
-  it("should create a rover and should be able to turn Right", () => {
+  it("turns Right", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("R")
     expect(position).toBe("0:0:E")
   })
 
-  it("should create a rover and should be able to move forward twice", () => {
+  it("moves forward twice", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("MM")
