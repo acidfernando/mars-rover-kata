43a714c52f571370ea9ba4b8cc9a9c320809bf29
Fernando García Hernández <86410308+acidfernando@users.noreply.github.com>
2024-03-15 15:49:01 +0000

    green: mars rover can move forward

--- src/MarsRover.spec.ts
+++ src/MarsRover.spec.ts
@@ -1,18 +1,18 @@
 import { describe, it, expect } from "vitest"
-import { MarsRover } from "./main.js"
+import { MarsRover } from "./MarsRover.js"
 
 describe(MarsRover, () => {
   it("should create a rover and move it", () => {
     const marsRover = new MarsRover()
 
-    const position = marsRover.execute("MRM")
-    expect(position).toBe("1:1:E")
+    const position = marsRover.execute("")
+    expect(position).toBe("0:0:N")
   })
 
   it("should create a rover and move it", () => {
     const marsRover = new MarsRover()
 
-    const position = marsRover.execute("MLM")
-    expect(position).toBe("9:1:W")
+    const position = marsRover.execute("M")
+    expect(position).toBe("0:1:N")
   })
 })
--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -2,6 +2,7 @@ export class MarsRover {
   constructor() {}
 
   public execute(command: string): string {
-    return "1:1:E"
+    if (command === "M") return "0:1:N"
+    return "0:0:N"
   }
 }
