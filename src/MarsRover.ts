987f61e7a6224cc1f39eddf16d4a18931f0d95ae
= <glezgalvan.adrian@gmail.com>
2024-03-15 15:56:13 +0000

    green: should create a rover and should be able to turn Right

--- src/MarsRover.ts
+++ src/MarsRover.ts
@@ -4,6 +4,7 @@ export class MarsRover {
   public execute(command: string): string {
     if (command === "M") return "0:1:N"
     if (command === "L") return "0:0:W"
+    if (command === "R") return "0:0:E"
     return "0:0:N"
   }
 }
