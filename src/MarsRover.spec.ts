8cc8b34715dbca8485845a42ea6167765949c5e0
Manuel Andrés Carrera Galafate <manuelcarrera@acidtango.com>
2024-03-15 15:52:55 +0000

    red: should create a rover and should be able to turn Left

--- src/MarsRover.spec.ts
+++ src/MarsRover.spec.ts
@@ -1,18 +1,25 @@
-import { describe, it, expect } from "vitest"
+import { describe, expect, it } from "vitest"
 import { MarsRover } from "./MarsRover.js"
 
 describe(MarsRover, () => {
-  it("should create a rover and move it", () => {
+  it("should create a Mars Rover in 0:0:N", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("")
     expect(position).toBe("0:0:N")
   })
 
-  it("should create a rover and move it", () => {
+  it("should create a rover and move it North", () => {
     const marsRover = new MarsRover()
 
     const position = marsRover.execute("M")
     expect(position).toBe("0:1:N")
+  })
+
+  it("should create a rover and should be able to turn Left", () => {
+    const marsRover = new MarsRover()
+
+    const position = marsRover.execute("L")
+    expect(position).toBe("0:0:W")
   })
 })
