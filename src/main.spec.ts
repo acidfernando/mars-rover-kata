180e405c04db74ed89aa97c3512de9f10ff5e565
= <glezgalvan.adrian@gmail.com>
2024-03-15 15:38:06 +0000

    red: test mars rover movement

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -8,4 +8,11 @@ describe(MarsRover, () => {
     const position = marsRover.execute("MRM")
     expect(position).toBe("1:1:E")
   })
+
+  it("should create a rover and move it", () => {
+    const marsRover = new MarsRover()
+
+    const position = marsRover.execute("MLM")
+    expect(position).toBe("9:1:W")
+  })
 })
